8b34a15a617628c31cf5e9e8f06390c1d0c81b42
Bellamkondakavya <162249500+Bellamkondakavya@users.noreply.github.com>
2026-05-16 16:45:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById("display");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let expression = display.value;
+
+    expression = expression.replace(/×/g, "*");
+    expression = expression.replace(/÷/g, "/");
+
+    display.value = Function("return " + expression)();
+
+  } catch {
+    display.value = "Error";
+  }
+}
+
+/* KEYBOARD SUPPORT */
+document.addEventListener("keydown", (e) => {
+  if (/[0-9+\-*/.]/.test(e.key)) {
+    append(e.key);
+  } else if (e.key === "Enter") {
+    calculate();
+  } else if (e.key === "Backspace") {
+    deleteLast();
+  } else if (e.key === "Escape") {
+    clearDisplay();
+  }
+});
